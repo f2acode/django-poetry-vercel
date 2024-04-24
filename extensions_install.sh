e104f0e89ac22d3e8f50cfed493c6b171ff2609a
Felipe Augusto <augustto.felipe@hotmail.com>
2024-04-24 17:48:31 -0300

    style: format and extensions

---
+++ extensions_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+code --install-extension ms-python.python
+code --install-extension batisteo.vscode-django
+code --install-extension ms-python.black-formatter
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.isort
+code --install-extension ms-python.pylint
